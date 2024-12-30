0a8db774be87adbe2a09a4c996c6f18160ae5e37
Kanchan Sharma <104859503+kanchancloud@users.noreply.github.com>
2024-12-30 20:10:54 +0530

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -20,13 +20,13 @@ https://www.figma.com/proto/wyqJtsflIsOcfCffhf5kbw/game-vde?node-id=1-1047&t=gs0
                     <div>
                         <a href=" https://www.figma.com/proto/0I1xKlBcGhmtWTQbpzZ9bE/dark-design-practice-project-)?node-id=3-2&t=FFvUj4IYiMh2uCCH-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1" target="_blank">
                             <img src="appart.png" alt={"img"} className={"bigImage"}/>
-                            <p>apartment rental apps</p>
+                            <p>practice project </p>
                         </a>
                     </div>
-                    <div>
-                        <a href=" https://www.figma.com/proto/0I1xKlBcGhmtWTQbpzZ9bE/dark-design-practice-project-)?node-id=3-2&t=FFvUj4IYiMh2uCCH-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1" target="_blank">
+                      <div>
+                        <a href="https://www.figma.com/proto/HZ5g9YVCdIKO1HErZTbGGe/selebs?node-id=3-5&t=FFvUj4IYiMh2uCCH-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=3%3A5" target="_blank">
                             <img src="appart.png" alt={"img"} className={"bigImage"}/>
-                            <p>apartment rental apps</p>
+                            <p>practice project </p>
                         </a>
                     </div>
                     <div>
@@ -45,9 +45,14 @@ https://www.figma.com/proto/wyqJtsflIsOcfCffhf5kbw/game-vde?node-id=1-1047&t=gs0
                         <img src="Screenshot 2023-05-29 191440.png" alt={"img"} className={"smallCash"}/>
                         <p>Recreate CashMonk app in figma</p>
                     </a>
-<a href="https://www.figma.com/proto/1uet1Ap1EnW4NZAEIk4NmA/Fitness-app?node-id=4-34&t=FFvUj4IYiMh2uCCH-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=4%3A34&show-proto-sidebar=1" target="_blank">
+                        <a href="https://www.figma.com/proto/1uet1Ap1EnW4NZAEIk4NmA/Fitness-app?node-id=4-34&t=FFvUj4IYiMh2uCCH-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=4%3A34&show-proto-sidebar=1" target="_blank">
                         <img src="Screenshot 2023-05-29 191440.png" alt={"img"} className={"smallCash"}/>
-                        <p>Recreate CashMonk app in figma</p>
+                        <p>Fitness app</p>
+                    </a>
+                    </a>
+                        <a href="https://www.figma.com/proto/2JqGBfwMF5xiRyJqkLLd2R/Food-Delivery-Mobile-Apps?node-id=556-190&t=FFvUj4IYiMh2uCCH-0&scaling=min-zoom&content-scaling=fixed&page-id=513%3A1881" target="_blank">
+                        <img src="Screenshot 2023-05-29 191440.png" alt={"img"} className={"smallCash"}/>
+                        <p>Food Delivery Mobile Apps</p>
                     </a>
                    </div>
                 </div>
